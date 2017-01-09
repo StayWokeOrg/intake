19a9ce1874030334c5e0770e1214feab60625a74
meganrm <meganrm@gmail.com>
2017-01-09 15:40:14 -0800

    linter error

--- public/scripts/Model/users.js
+++ public/scripts/Model/users.js
@@ -1,23 +1,23 @@
 function User(opts) {
-  for (key in opts) {
+  for (var key in opts) {
     this[key] = opts;
   }
 }
 
-User.prototype.insertNewUser = function () {
+User.prototype.insertNewUser = function insertNewUser() {
 // write this user into database
 };
 
 // FUNCTIONS THAT MAY BE USEFUL
-User.prototype.deleteUser = function () {
+User.prototype.deleteUser = function deleteUser() {
 // delete this user from database
 
 };
 
-User.prototype.updateUser = function () {
+User.prototype.updateUser = function updateUser() {
 // update this user from database
 };
 
 // LOOK UP FOR LOCATION OR INTERESTS?
-User.allInCategory = function (category) {
+User.allInCategory = function allInCategory(category) {
 };
