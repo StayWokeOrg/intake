97b1dd836f138e01aba93c1012e7dc2ce4fab007
meganrm <meganrm@gmail.com>
2017-01-09 15:21:22 -0800

    fixed some linter errors issues with func-names

--- public/scripts/Model/users.js
+++ public/scripts/Model/users.js
@@ -4,22 +4,20 @@ function User(opts) {
   }
 }
 
-
 User.prototype.insertNewUser = function () {
-  //write this user into database
+// write this user into database
 };
 
-//FUNCTIONS THAT MAY BE USEFUL
+// FUNCTIONS THAT MAY BE USEFUL
 User.prototype.deleteUser = function () {
-  //delete this user from database
+// delete this user from database
 
 };
 
 User.prototype.updateUser = function () {
-  //update this user from database
+// update this user from database
 };
 
-//LOOK UP FOR LOCATION OR INTERESTS?
+// LOOK UP FOR LOCATION OR INTERESTS?
 User.allInCategory = function (category) {
-
 };
