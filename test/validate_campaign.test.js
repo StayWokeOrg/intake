8e7564c10d0591f91d48494499ede2b21d7e49c2
Pascal <pascal@pascal.com>
2017-01-12 02:00:19 -0500

    test tweak

--- test/validate_campaign.test.js
+++ test/validate_campaign.test.js
@@ -5,18 +5,20 @@ const should = chai.should() // eslint-disable-line
 
 const validateCampaign = require('../src/validate_campaign')
 
-describe('#validateCampaign()', () => {
-  const examples = [
-    { value: null, description: 'null', expected: false },
-    { value: undefined, description: 'undefined', expected: false },
-    { value: '', description: 'empty string', expected: false },
-    { value: 'inauguration', expected: 'inauguration' },
-    { value: 'random', expected: 'unknown:random' },
-  ]
-  examples.forEach((example) => {
-    describe(`${example.value || example.description}`, () => {
-      it(`should return ${example.expected}`, () => {
-        validateCampaign(example.value).should.equal(example.expected)
+describe('validate_campaign:', () => {
+  describe('#validateCampaign()', () => {
+    const examples = [
+      { value: null, description: 'null', expected: false },
+      { value: undefined, description: 'undefined', expected: false },
+      { value: '', description: 'empty string', expected: false },
+      { value: 'inauguration', expected: 'inauguration' },
+      { value: 'random', expected: 'unknown:random' },
+    ]
+    examples.forEach((example) => {
+      describe(`${example.value || example.description}`, () => {
+        it(`should return ${example.expected}`, () => {
+          validateCampaign(example.value).should.equal(example.expected)
+        })
       })
     })
   })
