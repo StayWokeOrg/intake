20e2da672af4cd18fcd2d3141ee3e10d27513809
Doug Black <dougblackjr@gmail.com>
2017-01-16 18:32:50 -0500

    Testing for environment variables
    Tests for SESSION_SECRET, DATA_API, DATA_API_PORT, and DATA_API_TOKEN

--- src/index.js
+++ src/index.js
@@ -77,8 +77,17 @@ app.post('/sms', sms.dispatcher)
 app.post('/submit', web.submit)
 
 
-app.listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${app.get('port')}`)
-})
+// Break if vars not set
+if(process.env.SESSION_SECRET && process.env.DATA_API && process.env.DATA_API_PORT && process.env.DATA_API_TOKEN) { 
+  app.listen(app.get('port'), () => {
+    console.log(`Express server listening on port ${app.get('port')}`)
+  })
+}
+else { 
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.sendStatus(err.status || 500)
+  })
+}
 
 module.exports = app
