5298d4662e6ed6ba5e201b4267cfed9746dea7f0
Pascal <pascal@pascal.com>
2017-01-19 21:15:13 -0500

    updated message helper to handle multiple messages

--- src/controller/sms/message.js
+++ src/controller/sms/message.js
@@ -1,10 +1,10 @@
-// TWIML markup helper
-module.exports = function message(msg) {
-  return `
-    <Response>
-      <Message>
-        ${msg}
-      </Message>
-    </Response>
-  `
+/**
+ * message - TWIML markup helper
+ *
+ * @param  {type} ...items one or more pieces of text
+ * @return {TWIML} the items wrapped in TWIML markup
+ */
+module.exports = function message(...items) {
+  const messages = items.map(item => `<Message>${item}</Message>`)
+  return `<Response>${messages.join('')}</Response>`
 }
